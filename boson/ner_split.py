9401cb9c7c38e2e69358e2aca88c73b7b404271c
lollipop <24547008+HuHsinpang@users.noreply.github.com>
2020-07-10 08:22:27 +0800

    Update ner_split.py

--- boson/ner_split.py
+++ boson/ner_split.py
@@ -6,7 +6,8 @@
 #                                                                                #
 # ****************************************************************************** #
 
-# -*- coding: utf-8 -*-
+# !/usr/bin/python
+# -*-coding:utf-8 -*-
  
 import random
 import os
@@ -34,12 +35,14 @@ def paragraph_split(origin_file=None):
 
 def train_test_split(all_list=None, shuffle=False, ratio=0.8):
     assert len(all_list)>0
-    offset = int(len(all_list) * ratio)
+    offset1 = int(len(all_list) * ratio)
+    offset2 = (len(all_list)-offset1)//2 + offset1
     if shuffle:
         random.shuffle(all_list)    # 列表随机排序
-    train = all_list[:offset]
-    test = all_list[offset:]
-    return train, test
+    train = all_list[:offset1]
+    dev = all_list[offset1:offset2]
+    test = all_list[offset2:]
+    return train, dev, test
 
 
 def origin2tag(untagged_list=None):
@@ -64,7 +67,7 @@ def origin2tag(untagged_list=None):
                 else:               # 针对B、I、E标记
                     tagged_line_str += entity[0]+'/B_'+tag+' '
                     for j in range(1, len(entity)-1):
-                        tagged_line_str += entity[j]+'/I_'+tag+' '
+                        tagged_line_str += entity[j]+'/M_'+tag+' '
                     tagged_line_str += entity[-1]+'/E_'+tag+' '
             else:                   # 对于非实体O标记
                 tagged_line_str += line[i]+'/O '
@@ -86,24 +89,26 @@ def flatten2result(input_list=None, output_file=None):
 
 def main():
     # 针对vscode等不识别绝对路径的问题
-    cur_dir = os.chdir(os.path.dirname(__file__))
+    # cur_dir = os.chdir(os.path.dirname(__file__))
 
     # 将段落切分成句子
     with open('./data/BosonNLP_NER_6C.txt', 'r', encoding='utf-8') as boson_origin_file:
         sentences_list = paragraph_split(boson_origin_file)
 
     # 按8:2切分数据集为训练集、测试集
-    train_list, test_list = train_test_split(sentences_list, shuffle=True, ratio=0.8)
+    train_list, dev_list, test_list = train_test_split(sentences_list, shuffle=True, ratio=0.8)
 
     # 将实体标注转换为BIOES标注
-    tagged_train_list, tagged_test_list = origin2tag(train_list), origin2tag(test_list)
+    tagged_train_list, tagged_dev_list, tagged_test_list = origin2tag(train_list), origin2tag(dev_list), origin2tag(test_list)
 
     # 按照”字 tag\n“的格式，平铺所有数据,并保存
-    with open('./result/bosonner.train.bioes', 'w', encoding='utf-8') as train_file, open('./result/bosonner.test.bioes', 'w', encoding='utf-8') as test_file:
+    with open('./result/boson.train.bmes', 'w', encoding='utf-8') as train_file, \
+        open('./result/boson.dev.bmes', 'w', encoding='utf-8') as dev_file, \
+        open('./result/boson.test.bmes', 'w', encoding='utf-8') as test_file:
         flatten2result(tagged_train_list, train_file)
+        flatten2result(tagged_dev_list, dev_file)
         flatten2result(tagged_test_list, test_file)
     
 
 if __name__ == "__main__":
     main()
-
