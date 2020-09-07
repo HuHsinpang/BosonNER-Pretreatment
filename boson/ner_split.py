fb08d0c8a9afa6052b10ed7e65240e178c19a2ff
lollipop <24547008+HuHsinpang@users.noreply.github.com>
2020-09-07 17:23:52 +0800

    Update ner_split.py

--- boson/ner_split.py
+++ boson/ner_split.py
@@ -18,26 +18,23 @@ random.seed(0)
 
 # ****************************************************************************** #
 
-def paragraph_split(origin_file=None):
-    sentences_list, sent_end = [], ['；', '。', '！']
-    for line in origin_file.readlines():
-        sentence_items, sent_idx = re.split('([；。！])', line.strip()), 0  # 切分句子，保留分隔符
-        while sent_idx < len(sentence_items):
-            if sentence_items[sent_idx] not in sent_end:
-                if sent_idx<len(sentence_items)-2 and sentence_items[sent_idx+1] in sent_end:
-                    sentence = sentence_items[sent_idx]+sentence_items[sent_idx+1]
-                    sent_idx += 1
+def paragraph_split(tagged_paragraph_list=None):
+    sentences_list, sent_split = [], ["。/O", "！/O", "？/O", "；/O"]
+    for paragraph in tagged_paragraph_list:
+        temp = ''
+        for sent in re.split('([ 。 ！ ？ ；]/[O])', paragraph):
+            if sent not in sent_split:
+                temp += sent
+            else:
+                if temp!='':
+                    sentences_list.append(temp+sent)
+                    temp = ''
                 else:
-                    sentence = sentence_items[sent_idx]     # 针对在句尾且无结尾符的句子
-            else:                                           # 对于单个出现的结尾符
-                sentence = sentence_items[sent_idx]
-            sent_idx += 1
-            sentences_list.append(sentence.replace(' ', ''))
-    
+                    continue
     return sentences_list
 
 
-def train_test_split(all_list=None, shuffle=False, ratio=0.8):
+def file_split(all_list=None, shuffle=False, ratio=0.8):
     assert len(all_list)>0
     offset1 = int(len(all_list) * ratio)
     offset2 = (len(all_list)-offset1)//2 + offset1
@@ -49,36 +46,39 @@ def train_test_split(all_list=None, shuffle=False, ratio=0.8):
     return train, dev, test
 
 
-def origin2tag(untagged_list=None):
-    tagged_lines_list = []           # 存放标记后的数据
+def origin2tag(untagged_file=None):
+    tagged_paragraph_list = []          # 存放标记后的数据
 
-    for line in untagged_list:
-        # print(line)               # 用来脏数据处理
-        tagged_line_str, i = '', 0
-        while i<len(line):          # 对于每一句原始数据，逐字进行遍历
-            if line[i] == '{':      # 发现实体
+    for paragraph in untagged_file.readlines():
+        # print(line)               # 当出现错误时，打印进行脏数据处理
+        tagged_paragraph_str, i = '', 0
+        while i<len(paragraph.rstrip('\n')):          # 对于每一句原始数据，逐字进行遍历
+            if len(paragraph[i].strip()) == 0 or paragraph[i] == ' ' or paragraph[i]=='	' or paragraph[i]==' ':
+                i += 1
+            elif paragraph[i] == '{':      # 发现实体
                 i+=2
                 temp=""
-                while line[i]!='}':
-                    temp+=line[i]
+                while paragraph[i]!='}':
+                    temp+=paragraph[i]
                     i+=1
                 i+=2
 
-                tag, entity= temp.split(':')[0], temp.split(':')[1]
+                tag, entity= temp.split(':')[0], temp.split(':')[1].strip()
 
                 if len(entity)==1:  # 针对S标记
-                    tagged_line_str += entity+'/S_'+tag+' '
+                    tagged_paragraph_str += entity+'/S-'+tag+' '
                 else:               # 针对B、I、E标记
-                    tagged_line_str += entity[0]+'/B_'+tag+' '
+                    entity = entity.replace(' ', '_')
+                    tagged_paragraph_str += entity[0]+'/B-'+tag+' '
                     for j in range(1, len(entity)-1):
-                        tagged_line_str += entity[j]+'/M_'+tag+' '
-                    tagged_line_str += entity[-1]+'/E_'+tag+' '
-            elif line[i]!=' ':                   # 对于非实体O标记
-                tagged_line_str += line[i]+'/O '
-                i+=1
-        tagged_lines_list.append(tagged_line_str)
+                        tagged_paragraph_str += entity[j]+'/M-'+tag+' '
+                    tagged_paragraph_str += entity[-1]+'/E-'+tag+' '
+            else:                   # 对于非实体O标记
+                tagged_paragraph_str += paragraph[i]+'/O '
+                i += 1
+        tagged_paragraph_list.append(tagged_paragraph_str)
     
-    return tagged_lines_list
+    return tagged_paragraph_list
 
 
 def flatten2result(input_list=None, output_file=None):
@@ -97,22 +97,21 @@ def main():
 
     # 将段落切分成句子
     with open('./data/BosonNLP_NER_6C.txt', 'r', encoding='utf-8') as boson_origin_file:
-        sentences_list = paragraph_split(boson_origin_file)
+        tagged_paragraph_list = origin2tag(boson_origin_file)
+        sentences_list = paragraph_split(tagged_paragraph_list)
 
-    # 按8:2切分数据集为训练集、测试集
-    train_list, dev_list, test_list = train_test_split(sentences_list, shuffle=True, ratio=0.8)
+        # 按8:2切分数据集为训练集、测试集
+        train_list, dev_list, test_list = file_split(sentences_list, shuffle=True, ratio=0.8)
 
-    # 将实体标注转换为BIOES标注
-    tagged_train_list, tagged_dev_list, tagged_test_list = origin2tag(train_list), origin2tag(dev_list), origin2tag(test_list)
-
-    # 按照”字 tag\n“的格式，平铺所有数据,并保存
-    with open('./result/boson.train.bmes', 'w', encoding='utf-8') as train_file, \
-        open('./result/boson.dev.bmes', 'w', encoding='utf-8') as dev_file, \
-        open('./result/boson.test.bmes', 'w', encoding='utf-8') as test_file:
-        flatten2result(tagged_train_list, train_file)
-        flatten2result(tagged_dev_list, dev_file)
-        flatten2result(tagged_test_list, test_file)
+        # 按照”字 tag\n“的格式，平铺所有数据,并保存
+        with open('./result/boson.train.bmes', 'w', encoding='utf-8') as train_file, \
+            open('./result/boson.dev.bmes', 'w', encoding='utf-8') as dev_file, \
+            open('./result/boson.test.bmes', 'w', encoding='utf-8') as test_file:
+            flatten2result(train_list, train_file)
+            flatten2result(dev_list, dev_file)
+            flatten2result(test_list, test_file)
     
 
 if __name__ == "__main__":
     main()
+
